266750df0c32ccf181ce99cddde5b06d60d02d11
Samir899 <sameerkmr823@gmail.com>
2025-12-30 08:23:09 +0530

    Initial Commit

---
+++ FuelPay/FuelPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelPayApp.swift
+//  FuelPay
+//
+//  Created by Samir Kumar on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FuelPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
